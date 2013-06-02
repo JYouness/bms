d0d65cce37b4493b800dc380e18fdb8fe48833bb
David Bowen <david@lifeofabowen.com>
2013-06-03 00:37:18 +0100

    Added route to view blog post

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,12 @@
     $baseurl = Config::get('empower::baseurl');
 
     Route::group(array('before' => 'auth|magicaurp', 'prefix' => $baseurl), function () {
-
         Route::resource('series', 'Sorora\\Bms\\Controllers\\SeriesController');
-
         Route::resource('posts', 'Sorora\\Bms\\Controllers\\PostsController');
+    });
 
+    Route::group(array('prefix' => Config::get('bms::prefix')), function () {
+        $prefix = Config::get('bms::prefix');
+        $prefix = ($prefix) ? Config::get('bms::prefix').'.' : null;
+        Route::get('{slug}', array('as' => $prefix.'blog.show', 'uses' => 'Sorora\\Bms\\Controllers\\BlogController@show'));
     });
