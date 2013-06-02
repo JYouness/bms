d463e2d4365ac4454b00aba4fbc3720f1ec0cb11
David Bowen <david@lifeofabowen.com>
2013-06-02 20:51:00 +0100

    Added routes to posts and series

--- src/routes.php
+++ src/routes.php
@@ -1,2 +1,10 @@
 <?php
     $baseurl = Config::get('empower::baseurl');
+
+    Route::group(array('before' => 'auth|magicaurp', 'prefix' => $baseurl), function () {
+
+        Route::resource('series', 'Sorora\\Bms\\Controllers\\SeriesController');
+
+        Route::resource('posts', 'Sorora\\Bms\\Controllers\\PostsController');
+
+    });
