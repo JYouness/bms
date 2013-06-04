ec3f3c4fd9a474ec098874a97f34a68b60ffa6c3
David Bowen <david@lifeofabowen.com>
2013-06-04 12:53:26 +0100

    Added tags/categories on listed posts

--- src/views/blog/_listed_post.blade.php
+++ src/views/blog/_listed_post.blade.php
@@ -3,9 +3,19 @@
         <header>
             <h2>{{ HTML::linkRoute('blog.post', $post->title, array($post->slug)) }}</h2>
         </header>
+        <small>
+            @if($post->updated_at > $post->created_at)
+                Last updated {{ $post->getDateDiffForHumans('updated_at') }}, originally
+            @endif
+            Posted {{ $post->getDateDiffForHumans('created_at') }}
+            @include('bms::blog._post_categories')
+        </small>
         <section>
             {{ Str::words($post->content) }}
         </section>
-        <small>{{ HTML::linkRoute('blog.post', 'Read more', array($post->slug)) }}</small>
+        <small>
+            {{ HTML::linkRoute('blog.post', 'Read more', array($post->slug)) }}<br />
+            @include('bms::blog._tags')
+        </small>
     </article>
 </li>
